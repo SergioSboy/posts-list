107640ab80243e1a46228466c3336d36af6937f5
Sergio <seregaant71@gmail.com>
2024-06-08 13:59:16 +0300

    Добавил на страницу о посте, загрузку информации из сервера по id из параметров. Добавил хуки и Loader.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
+import {useFetching} from "../hooks/useFetching";
+import PostService from "../API/PostService";
+import Loader from "../components/UI/Loader/Loader";
+
 const PostPage = () => {
     const params = useParams();
+    const [post, setPost] = useState({});
+    const [fetchedPostById, isLoading, error] = useFetching(async (id) => {
+        const response = await PostService.getById(params.id)
+        setPost(response.data);
+    })
+
+
+    useEffect(() => {
+        fetchedPostById(params.id)
+    }, []);
+
     return (
         <div>
             <h1>Вы на странице поста c ID = {params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                : <div>{post.id}. {post.title}</div>
+            }
+            <h1>
+                Комментарии
+            </h1>
+
         </div>
     );
 };
