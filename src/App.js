a66ebe9ce29a26d58f5d7ceaf8162d866c7801ee
Sergio <seregaant71@gmail.com>
2024-06-08 13:27:14 +0300

    Сделал рефакторинг маршрутизации, перенес в отдельную компоненту AppRouter.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import './styles/App.css'
-import {Link, Navigate, Route, Routes} from "react-router-dom";
-import Posts from "./pages/Posts";
-import About from "./pages/About.";
 import Navbar from "./components/UI/navbar/Navbar";
-import Error from "./pages/Error";
+import AppRouter from "./components/AppRouter/AppRouter";
 
 // Добавил комментарии
 
@@ -13,13 +10,7 @@ function App() {
     return (
         <div>
             <Navbar/>
-            <Routes>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/posts" element={<Posts/>}/>
-                <Route path="/error" element={<Error/>}/>
-                <Route path="/*" element={<Navigate to="/error" replace />} />
-            </Routes>
-
+            <AppRouter/>
         </div>
     )
 }
